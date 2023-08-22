3818971af249a73114690672401e24fd2586a083
Ncg233 <2903824519@qq.com>
2023-08-22 14:59:40 +0800

    first

--- src/main/java/com/ncg233/teasdelight/TeaDelight.java
+++ src/main/java/com/ncg233/teasdelight/TeaDelight.java
@@ -46,7 +46,6 @@ public class TeaDelight
         TDRecipeSerializers.RECIPE_SERIALIZERS.register(modEventBus);
         modEventBus.addListener(this::addCreative);
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -54,12 +53,6 @@ public class TeaDelight
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
 
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // Add the example block item to the building blocks tab
